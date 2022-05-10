33750fccd0618dc9a86408953f130ea851daaaa5
Ishaan Sharma <ishaansharmahfx@gmail.com>
2022-05-10 23:41:35 +0530

    Initial Commit

---
+++ helloworld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Ishaan Sharma  on 10/05/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
