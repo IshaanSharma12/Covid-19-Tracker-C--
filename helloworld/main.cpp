5fdb28264812272e1fe55d49edfaeabe30eea17f
IshaanSharma12 <75562836+IshaanSharma12@users.noreply.github.com>
2022-05-15 17:03:44 +0530

    Update main.cpp

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -1,14 +1,48 @@
-//
+ //
 //  main.cpp
 //  helloworld
 //
 //  Created by Ishaan Sharma  on 10/05/22.
 //
+#include<iostream>
+using namespace std;
+int main()
+{
 
-#include <iostream>
+    // This is Covid 19 Verification App for Residents of Ottawa. "
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+    // Name Details
+    
+std::cout<<"Welcome to Covid Report Station";
+    char name;
+    std::cin>>name;
+    std::cout<<"Thanks for your name details";
+    
+    //Covid Related Health Information
+    int temp;
+    int days;
+    std::cout<<"/n What is your body temp?";
+   std::cin >>temp;
+    std::cout<<"How many days?";
+    std::cin>>days;
+       if (temp>98 &&days>33)
+            std::cout<<"Please get tested for Covid 19";
+       }
+//Thank you for using this application.
+                 
+    
+    
+    
+    
+    
+    //
+    
+    
+    
+    
+    
+    
+
+
+
+
